55f4803b86b404580d2fe367e7127afcb6a81bb3
Andrew Schutt <andrew@interactiveshift.com>
2016-11-21 12:54:25 -0600

    test driven List Comprehensions for elixir

--- lib/elixir_card_deck.ex
+++ lib/elixir_card_deck.ex
@@ -1,5 +1,6 @@
 defmodule ElixirCardDeck do
   def make_deck do
-    []
+    for rank <- [:a, 2, 3, 4, 5, 6, 7, 8, 9, 10, :j, :q, :k],
+        suit <- [:spades, :clubs, :diamonds, :hearts], do: { :card, rank, suit }
   end
 end
